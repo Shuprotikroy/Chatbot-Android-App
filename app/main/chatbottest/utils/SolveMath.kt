71c2d77412d32d1cb656bef62a8d850012c81299
Shuprotikroy <70252750+Shuprotikroy@users.noreply.github.com>
2021-05-11 13:16:42 +0530

    Create SolveMath.kt

---
+++ app/main/chatbottest/utils/SolveMath.kt
@@ -0,0 +1,39 @@
+package learn.codeacademy.chatbottest.utils
+
+import android.util.Log
+
+//object class that helps bot solve maths equations
+object SolveMath {
+
+    fun solveMath(equation: String) : Int{
+
+        val newEquation = equation.replace(" ", "")
+        Log.d("Math", newEquation)
+
+        return when {
+            newEquation.contains("+") -> {
+                val split = newEquation.split("+")
+                val result = split[0].toInt() + split[1].toInt()
+                result
+            }
+            newEquation.contains("-") -> {
+                val split = newEquation.split("-")
+                val result = split[0].toInt() - split[1].toInt()
+                result
+            }
+            newEquation.contains("*") -> {
+                val split = newEquation.split("*")
+                val result = split[0].toInt() * split[1].toInt()
+                result
+            }
+            newEquation.contains("/") -> {
+                val split = newEquation.split("/")
+                val result = split[0].toInt() / split[1].toInt()
+                result
+            }
+            else -> {
+                0
+            }
+        }
+    }
+}
