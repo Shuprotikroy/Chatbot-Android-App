5836074fba8dd1bf809733ca0a2aae2f4cfcb0fd
Shuprotikroy <70252750+Shuprotikroy@users.noreply.github.com>
2021-05-11 13:17:06 +0530

    Create Time.kt

---
+++ app/main/chatbottest/utils/Time.kt
@@ -0,0 +1,15 @@
+package learn.codeacademy.chatbottest.utils
+
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+//object class that returns time
+object Time {
+
+    fun timeStamp() : String {
+        val timeStamp = Timestamp(System.currentTimeMillis())
+        val sdf = SimpleDateFormat("HH:mm")
+        val time = sdf.format(Date(timeStamp.time))
+        return time.toString()
+    }
+}
